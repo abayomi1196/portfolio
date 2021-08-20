6665289faba6a39c1e9616ca570957860b611e95
Abayomi Ayodele <samabayomi301@gmail.com>
2021-08-20 14:30:50 +0100

    style: footer section

--- sections/footer/Footer.styled.ts
+++ sections/footer/Footer.styled.ts
@@ -1,1 +1,29 @@
 import tw, { styled, css } from 'twin.macro';
+
+export const FooterWrapper = tw.div`mt-24 pb-36`;
+
+export const Container = tw.div`max-w-4xl px-4 mx-auto text-gray-800`;
+
+export const Line = tw.div`pb-8 mb-2 border-t-2 border-gray-300`;
+
+export const FooterContents = styled.div(() => [
+  tw`
+    flex flex-col justify-between
+    lg:(flex-row)  
+  `,
+
+  css`
+    & > div {
+      ${tw`pt-2 space-x-6 font-medium lg:pt-0`}
+
+      a {
+        ${tw`
+        transition-colors 
+        hover:text-sky-500
+        focus:(text-sky-500 outline-none) 
+        focus-visible:(ring-2 ring-offset-2 ring-sky-500 ring-opacity-60 outline-none)
+        `}
+      }
+    }
+  `,
+]);
--- sections/footer/Footer.tsx
+++ sections/footer/Footer.tsx
@@ -1,5 +1,39 @@
+import {
+  Container,
+  FooterWrapper,
+  FooterContents,
+  Line,
+} from './Footer.styled';
+
 const Footer = () => {
-  return <div>hello from Footer</div>;
+  return (
+    <FooterWrapper>
+      <Container>
+        <Line />
+
+        <FooterContents>
+          <p>Built with Next.js, TailwindCSS and Styled Components</p>
+
+          <div>
+            <a
+              href="https://twitter.com/SirKamara"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Twitter
+            </a>
+            <a
+              href="https://github.com/AbayomiAyodele"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </div>
+        </FooterContents>
+      </Container>
+    </FooterWrapper>
+  );
 };
 
 export default Footer;
