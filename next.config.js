a2106810781d7e1050f46c8f6037885d4c595d72
Abayomi Ayodele <samabayomi301@gmail.com>
2021-08-20 23:00:20 +0100

    fix: move images to cloudinary

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    loader: 'cloudinary',
+    domains: ['res.cloudinary.com'],
+  },
   webpack: (config) => {
     // unset client side js that only works server-side
     config.resolve.fallback = { fs: false, module: false };
--- sections/projects/projectData.ts
+++ sections/projects/projectData.ts
@@ -1,7 +1,8 @@
 export const projects = [
   {
     title: 'TEF Restaurant',
-    image: '/projects/tef-preview.png',
+    image:
+      'https://res.cloudinary.com/ayo-clodinary/image/upload/v1629496456/tef-preview_gzgwkh.png',
     stack: ['React', 'Redux', 'Axios', 'Styled Components'],
     url: 'https://food.tef.ng',
     text: 'Food ordering application built for Tamuno Exalted Fingers (TEF) Restaurant. I was tasked primarily with connecting to the backend and implementing general application functionality.',
@@ -9,21 +10,24 @@ export const projects = [
 
   {
     title: 'Propexx Admin Console',
-    image: '/projects/propexx-preview.png',
+    image:
+      'https://res.cloudinary.com/ayo-clodinary/image/upload/v1629496452/propexx-preview_mxqkww.png',
     stack: ['React', 'Redux', 'Axios'],
     url: 'https://admin.propexx.com',
     text: 'Admin Console for a Property Management Dashboard Application, used primarily to manage staff, agents, customers, properties and more',
   },
   {
     title: 'Netflix Clone',
-    image: '/projects/netflix-preview.png',
+    image:
+      'https://res.cloudinary.com/ayo-clodinary/image/upload/v1629496485/netflix-preview_l1zrjm.png',
     stack: ['React', 'SASS', 'Firebase'],
     url: 'https://ayosnetflixclone2.netlify.app/',
     text: "A beginner's attempt at cloning the popular streaming service. built primarily to master basic to intermediate React topics, also an introduction to Firebase",
   },
   {
     title: 'Countries of the world.',
-    image: '/projects/countries-preview.png',
+    image:
+      'https://res.cloudinary.com/ayo-clodinary/image/upload/v1629496452/countries-preview_f53inu.png',
     stack: ['React', 'SASS'],
     url: 'https://ayoscountries2.netlify.app/',
     text: 'A front-end challenge that required me to integrate with the REST Countries API to pull country data and display it like in the designs.',
