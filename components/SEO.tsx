4b8f4bf860f855a55d4b4428f80c390acf53e00a
Abayomi Ayodele <samabayomi301@gmail.com>
2021-08-22 20:31:35 +0100

    chore: update SEO

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -2,33 +2,50 @@ import Head from 'next/head';
 
 const SEO = () => (
   <Head>
-    <title>Abayomi Ayodele</title>
+    <title>Abayomi Ayodele | Frontend Developer &amp; NBA Enthusiast</title>
     <meta name="description" content="Frontend/React Developer" />
 
-    {/* site title */}
-    <meta property="og:title" content="Abayomi Ayodele" key="ogtitle" />
-
-    {/* site desc */}
+    {/* Open Graph SEO */}
+    <meta
+      property="og:title"
+      content="Abayomi Ayodele | Frontend Developer &amp; NBA Enthusiast"
+      key="ogtitle"
+    />
     <meta
       property="og:description"
       content="Frontend/React Developer"
       key="ogdesc"
     />
-
-    {/* seo url */}
     <meta
       property="og:url"
       content="https://portfolio-ayodele.vercel.app/"
       key="ogurl"
     />
-
-    {/* seo img */}
+    <meta property="og-locale" content="en-NG" />
+    <meta property="og-type" content="website" />
     <meta property="og:image" content="/horse.jpg" key="ogimage" />
-
-    {/* seo site_name */}
     <meta property="og:site_name" content="Abayomi Ayodele" key="ogsitename" />
+    {/* End of Open Graph SEO */}
 
-    {/* site logo */}
+    {/* Twitter SEO */}
+    <meta name="twitter:card" content="/horse.jpg" />
+    <meta name="twitter:site" content="Abayomi Ayodele" />
+    <meta
+      name="twitter:domain"
+      content="https://portfolio-ayodele.vercel.app/"
+    />
+    <meta
+      name="twitter:title"
+      content="Abayomi Ayodele | Frontend Developer &amp; NBA Enthusiast"
+    />
+
+    <meta name="twitter:description" content="Frontend/React Developer" />
+
+    <meta name="twitter:image" content="/horse.jpg" />
+
+    <meta name="twitter:url" content="https://portfolio-ayodele.vercel.app/" />
+    {/* End of Twitter SEO */}
+
     <link rel="icon" href="/icon.png" />
   </Head>
 );
