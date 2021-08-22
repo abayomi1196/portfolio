eb79e67d63387ffa73909b9b718292a835e38412
Abayomi Ayodele <samabayomi301@gmail.com>
2021-08-22 21:48:59 +0100

    chore: Twitter Cards SEO

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -27,20 +27,19 @@ const SEO = () => (
     />
     <meta property="og-locale" content="en-NG" />
     <meta property="og-type" content="website" />
-    <meta property="og:image" content="/horse.jpg" key="ogimage" />
+    <meta
+      property="og:image"
+      content="https://res.cloudinary.com/ayo-clodinary/image/upload/v1629665194/78866624_y7yqv0.jpg"
+      key="ogimage"
+    />
     <meta property="og:site_name" content="Abayomi Ayodele" key="ogsitename" />
     {/* End of Open Graph SEO */}
 
     {/* Twitter SEO */}
     <meta name="twitter:card" content="summary" />
 
-    <meta name="twitter:site" content="Abayomi Ayodele" />
+    <meta name="twitter:site" content="@sirkamara" />
 
-    <meta name="twitter:creator" content="@sirkamara" />
-    <meta
-      name="twitter:domain"
-      content="https://portfolio-ayodele.vercel.app/"
-    />
     <meta
       name="twitter:title"
       content="Abayomi Ayodele | Frontend/React Developer"
@@ -48,15 +47,18 @@ const SEO = () => (
 
     <meta
       name="twitter:description"
-      content="I am a Frontend (React) developer, with growing experience in Modern Web Development. I am particularly interested in client-side web development."
+      content="I am a Frontend (React) developer, with growing experience in Modern Web Development."
     />
 
     <meta
       name="twitter:image"
-      content="https://avatars.githubusercontent.com/u/78866624?v=4"
+      content="https://res.cloudinary.com/ayo-clodinary/image/upload/v1629665194/78866624_y7yqv0.jpg"
     />
 
-    <meta name="twitter:url" content="https://portfolio-ayodele.vercel.app/" />
+    <meta
+      name="twitter:image:alt"
+      content="Bojack Horseman. I love that show."
+    />
     {/* End of Twitter SEO */}
 
     <link rel="icon" href="/icon.png" />
